3a0c2bc1156325418ba0a1ee98a26df38bf56bdd
文竹 <hengxinsky@dingtalk.com>
2022-02-07 23:27:41 +0800

    错误上报

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import ReactDOM from 'react-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 
+import { request } from '@/utils';
 import RouterComponents from '@/routers/router';
 
-import reportWebVitals from './reportWebVitals';
 import './index.less';
+
+window.onerror = function (message) {
+  request({
+    url: `api/log?error=${message}`,
+    method: 'GET',
+  });
+};
 
 ReactDOM.render(
   <ConfigProvider locale={zhCN}>
@@ -13,8 +20,3 @@ ReactDOM.render(
   </ConfigProvider>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
